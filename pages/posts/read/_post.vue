46f84677f1fce021a2d672a69446d8475a032ee1
zoz0312@naver.com <zoz0312@naver.com>
2019-11-06 15:53:27 +0900

    post read design fix

--- pages/posts/read/_post.vue
+++ pages/posts/read/_post.vue
@@ -2,18 +2,21 @@
 	<v-card
 		class="mx-auto pa-5"
 	>
-		<v-text-field
-			v-model="items.title"
-			label="제목"
-			class="d-inline-block text-truncate"
-		></v-text-field>
-		<v-text-field
-			v-model="items.writer"
-			label="작성자"
-		></v-text-field>
+		<div id="post-category">
+			{{ category }}
+		</div>
+		<div id="post-title">
+			{{ items.title }}
+		</div>
+		<div id="post-writer">
+			{{ items.writer }}
+		</div><div id="post-date">
+			&nbsp;|&nbsp;{{ dateParse(items.createDate) }}
+		</div>
+		<v-divider class="my-5"></v-divider>
 		<div
 			v-html="items.content"
-			class="d-inline-block text-truncate"
+			max-width="100%"
 		></div>
 	</v-card>
 </template>
@@ -29,15 +32,56 @@ export default {
 	},
 	data () {
 		return {
-			items: {}
+			items: {},
+			category: ''
 		}
 	},
 	mounted () {
 		this.$http.post('/post/' + this.post).then((result) => {
 			this.items = Object.assign({}, result.data.data);
+			this.$http.post('/category/' + this.items.categoryId).then((result) => {
+				this.category = result.data.data.title;
+			})
 		}).catch((err) => {
 			console.log('err', err);
 		})
 	}
 }
 </script>
+
+<style>
+#post-category {
+	color: #666666;
+	font-size: 1rem;
+	text-align: left;
+	font-weight: 400;
+}
+#post-title {
+	color: #428bca;
+	font-size: 2rem;
+	text-align: left;
+	font-weight: 300;
+}
+#post-writer {
+	display: inline-block;
+	color: #000000;
+	font-size: 0.9rem;
+	text-align: left;
+	font-weight: 200;
+}
+#post-date {
+	display: inline-block;
+	color: #666666 ;
+	font-size: 0.9rem;
+	font-weight: 300;
+}
+hr {
+	display: block;
+	flex: 1 1 0px;
+	max-width: 100%;
+	max-height: 0px;
+	border-color: rgba(0, 0, 0, 0.3);
+	border-width: thin 0 0 0;
+	margin: 1rem 0;
+}
+</style>
