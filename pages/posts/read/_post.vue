c8bf90af8319cf18ba08832cc9b342735918ee16
zoz0312@naver.com <zoz0312@naver.com>
2019-11-07 22:10:19 +0900

    조회수 Info Add

--- pages/posts/read/_post.vue
+++ pages/posts/read/_post.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-card
-		class="mx-auto pa-5"
+		class="mx-auto pa-5 "
 	>
 		<div id="post-category">
 			{{ category }}
@@ -8,10 +8,14 @@
 		<div id="post-title">
 			{{ items.title }}
 		</div>
-		<div id="post-writer">
-			{{ items.writer }}
-		</div><div id="post-date">
-			&nbsp;|&nbsp;{{ dateParse(items.createDate) }}
+		<div class="d-flex">
+			<div id="post-writer">
+				{{ items.writer }}
+			</div><div id="post-date" class="pb-3 mr-auto">
+				&nbsp;|&nbsp;{{ dateParse(items.createDate) }}
+			</div><div id="post-hit">
+				조회수 : {{ items.hitCount }}
+			</div>
 		</div>
 		<v-divider class="my-5"></v-divider>
 		<div
@@ -37,15 +41,13 @@ export default {
 		}
 	},
 	mounted () {
-		this.$http.post('/api/postHit', { 'postId': this.post }).then((result) => {
-			console.log('HIT POST!!!');
-		});
+		this.$http.post('/api/postHit', { 'postId': this.post });
 
 		this.$http.post('/post/' + this.post).then((result) => {
 			this.items = Object.assign({}, result.data.data);
-			this.$http.post('/category/' + this.items.categoryId).then((result) => {
-				this.category = result.data.data.title;
-			})
+			return this.$http.post('/category/' + this.items.categoryId);
+		}).then((result) => {
+			this.category = result.data.data.title;
 		}).catch((err) => {
 			console.log('err', err);
 		})
@@ -67,16 +69,20 @@ export default {
 	font-weight: 300;
 }
 #post-writer {
-	display: inline-block;
 	color: #000000;
 	font-size: 0.9rem;
 	text-align: left;
 	font-weight: 200;
 }
 #post-date {
-	display: inline-block;
 	color: #666666 ;
 	font-size: 0.9rem;
+	font-weight: 300;
+}
+#post-hit {
+	color: #666666;
+	font-size: 0.9rem;
+	text-align: right;
 	font-weight: 300;
 }
 hr {
