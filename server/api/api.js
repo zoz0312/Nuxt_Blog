5e605c9831bb9e473462274bc49ad17c299ae137
zoz0312@naver.com <zoz0312@naver.com>
2019-11-08 16:33:33 +0900

    Hit Count bug fix

--- server/api/api.js
+++ server/api/api.js
@@ -10,7 +10,7 @@ const writing = require('../../models/post');
 require('moment-timezone');
 moment.tz.setDefault("Asia/Seoul");
 
-let ip, postId, curDate;
+let ip, postId;
 
 router.post('/postHit', (req, res, next) => {
 	ip = req.headers['x-forwarded-for'] ||
@@ -18,18 +18,14 @@ router.post('/postHit', (req, res, next) => {
 		req.socket.remoteAddress ||
 		req.connection.socket.remoteAddress;
 	postId = req.body.postId;
-	curDate = moment().format('YYYY-MM-DD');
+	const curDate = moment().format('YYYY-MM-DD');
 
 	postHit.find({
 		$and: [{'postId': postId, 'hitIp': ip}]
 	}).then( postHit => {
-		let arr = [];
 		// Check Hit IP & Date
 		if( postHit !== null && postHit.length !== 0 ){
-			if( !postHit.isArray ){
-				arr.push(postHit);
-			}
-			const chkDate = arr.filter( obj => {
+			const chkDate = postHit.some( obj => {
 				return obj.hitDate === curDate;
 			})
 			if( chkDate ){
@@ -51,7 +47,7 @@ router.post('/postHit', (req, res, next) => {
 	let schm = new postHit();
 	schm.hitIp = ip;
 	schm.postId = postId;
-	schm.curDate = curDate;
+	schm.hitDate = moment().format('YYYY-MM-DD');
 	schm.save().then(() => {
 		return writing.updateOne({ '_id':postId }, {
 			$inc: { hitCount: 1 }
