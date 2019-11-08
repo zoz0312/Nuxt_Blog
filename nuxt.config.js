045af284641ad7253ddb9569436a991ae19ca52f
zoz0312@naver.com <zoz0312@naver.com>
2019-11-09 01:29:22 +0900

    test chart add

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+		{ src : '~/plugins/vue-apexchart.js', ssr : false },
   ],
   /*
   ** Nuxt.js dev-modules
@@ -83,10 +84,26 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    vendor : [
+			'vue-apexchart.js'
+		],
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      const vueLoader = config.module.rules.find(
+        rule => rule.loader === "vue-loader"
+      );
+      vueLoader.options.transformToRequire = {
+        img: "src",
+        image: "xlink:href",
+        "b-img": "src",
+        "b-img-lazy": ["src", "blank-src"],
+        "b-card": "img-src",
+        "b-card-img": "img-src",
+        "b-carousel-slide": "img-src",
+        "b-embed": "src"
+      };
     }
   },
   serverMiddleware: [
